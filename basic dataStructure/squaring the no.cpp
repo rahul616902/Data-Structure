1130ff6322747aa87987adc63028073686c7cff2
Rahul Kumar <136741641+rahul616902@users.noreply.github.com>
2024-11-09 12:33:41 +0530

    Create squaring the no.cpp

---
+++ basic dataStructure/squaring the no.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+		long long  n; // for squaring the no
+		cin>>n;
+		
+		cout<<n*n<<endl;
+	}
+	return 0;
+}
+
+// always remember for sqaring use long long 
