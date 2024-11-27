62157c00dc3f69ddeb9f200c00a1b2cff2fe570d
Rahul Kumar <136741641+rahul616902@users.noreply.github.com>
2024-11-28 02:46:39 +0530

    done

---
+++ hashmap/frequency_of_element.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count the frequency of all elements from 1 to N in the array.
+    vector<int> frequencyCount(vector<int>& arr) {
+       map<int, int> mp;
+
+    for(int i=0;i<arr.size();i++){
+        mp[arr[i]]++;
+    }
+
+    vector<int> res;
+
+    for(int i=0;i<arr.size();i++){
+        if(mp.find(i+1)!=mp.end()){
+        auto it = mp.find(i+1);
+            res.push_back(it->second);
+        }else{
+            res.push_back(0);
+        }
+    }
+    
+    return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.frequencyCount(a);
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no duplicates are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl; // Ensure new line after each test case output
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
