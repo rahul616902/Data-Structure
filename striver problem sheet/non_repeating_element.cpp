d2e4cd8ac6e3c0e41331cf1a7254613be99c7383
Rahul Kumar <136741641+rahul616902@users.noreply.github.com>
2024-11-28 19:16:24 +0530

    Create non_repeating_element.cpp

---
+++ striver problem sheet/non_repeating_element.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int firstNonRepeating(vector<int>& arr) {
+        // Complete the function
+        unordered_map<int,int>mp;
+        
+        for(auto it:arr){
+            
+            mp[it]++;
+        }
+      
+      for(int i=0; i<arr.size();i++){
+          
+          if(mp[arr[i]] == 1){
+              return arr[i];
+          }
+      }
+      
+      return 0;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        cout << ob.firstNonRepeating(nums) << endl;
+        cout << "~\n";
+    }
+}
+
+// } Driver Code Ends
